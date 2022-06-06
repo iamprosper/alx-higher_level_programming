a8fe9a8a9b96c3c73adbd38e554e4fbb193337d2
iamprosper <thinkpadprosper@gmail.com>
2022-06-06 15:29:47 +0000

    No_c and matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    i = 0
+    matrix_len = len(matrix)
+    for i in range(0, matrix_len):
+        # print(matrix[i])
+        values = len(matrix[i])
+        j = 0
+        while(j < values):
+            if (j + 1 < values):
+                print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]))
+            j += 1
