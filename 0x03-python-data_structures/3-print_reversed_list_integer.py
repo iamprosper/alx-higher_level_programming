2debe840eb898494a54c9aacf0d6b41312e5f4f8
iamprosper <thinkpadprosper@gmail.com>
2022-06-06 13:35:43 +0000

    Print reversed list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    i = 1
+    list_size = len(my_list)
+    while (i <= list_size):
+        print("{:d}".format(my_list[-i]))
+        i += 1
