ba7f386ea8260ce742447494068d7f17d1cc578d
iamprosper <thinkpadprosper@gmail.com>
2022-06-06 23:28:06 +0000

    Checking#2

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,8 @@
 def max_integer(my_list=[]):
     if (my_list == []):
         return None
-    max_list = my_list[1]
+    max_list = my_list[0]
+
     for i in range(len(my_list)):
         if (my_list[i] > max_list):
             max_list = my_list[i]
