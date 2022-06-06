f25938a944a531597ef41e66bbca5e227e991f43
iamprosper <thinkpadprosper@gmail.com>
2022-06-06 20:14:30 +0000

    Adding tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a_len = len(tuple_a)
+    tuple_b_len = len(tuple_b)
+
+    if(tuple_a_len > 0 and tuple_a_len < 2):
+        tuple_a = (tuple_a[0], 0)
+    elif(tuple_a_len == 0):
+        tuple_a = (0, 0)
+    elif(tuple_a_len > 2):
+        tuple_a = (tuple_a[0], tuple_a[1])
+
+    if(tuple_b_len > 0 and tuple_b_len < 2):
+        tuple_b =  (tuple_b[0], 0)
+    elif(tuple_b_len == 0):
+        tuple_b = (0, 0)
+    elif(tuple_b_len > 2):
+        tuple_b =  (tuple_b[0], tuple_b[1])
+
+    add_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (add_tuple)
