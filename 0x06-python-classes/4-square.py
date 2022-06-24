1ba9c1ab5834bb3d5193368aedcfeb8d39aa07ac
iamprosper <thinkpadprosper@gmail.com>
2022-06-24 18:11:30 +0000

    Task 4, private size attribute with property

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+"""
+    A module representing a single Square class with:
+    -size attribute which is a private instance attribute
+    The size attribute is validated
+"""
+
+
+class Square:
+
+    """A Square class"""
+
+    def __init__(self, size=0):
+
+        """Default constructor
+
+        Args:
+            size (int): The first parameter
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Size guetter
+
+        Returns:
+            The value of the square size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Size setter
+
+        Args:
+            size (int): The first parameter
+        """
+        if(isinstance(size, int)):
+            if(size < 0):
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+        self.__size = size
+
+    def area(self):
+        """Square area calculator
+
+        Returns:
+            The area of the square
+        """
+        return(self.__size ** 2)
