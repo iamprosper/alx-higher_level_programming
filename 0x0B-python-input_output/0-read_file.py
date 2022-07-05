f7d84f54cb54e6bb2aeb24fafaff99a0ad178b6a
iamprosper <thinkpadprosper@gmail.com>
2022-07-05 21:57:13 +0000

    Task 0 checking#2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" Reading module containing one single function"""
+
 def read_file(filename=""):
+    """ A function with print the content of a file to stdout
+        Args:
+            -filename: The file's name
+    """
     with open(filename) as f:
         for line in f:
             print(line, end="")
