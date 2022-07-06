b7836e6bcede598255a9acb2cf57a3faaf8c1cd5
iamprosper <thinkpadprosper@gmail.com>
2022-07-06 13:37:02 +0000

    Task1 truncating/creating file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,9 +10,16 @@ def write_file(filename="", text=""):
             -filename: The file's name
             -text: The content to add
     """
-    with open(filename, 'w', encoding="utf-8") as f:
-        f.seek(0)
-        ch = f.write(text)
-        f.truncate()
-        f.close()
+    try:
+        with open(filename, 'r+', encoding="utf-8") as f:
+            f.seek(0)
+            ch = f.write(text)
+            f.truncate()
+            f.close()
+            print("File contents updated")
+    except FileNotFoundError as fe:
+        with open(filename, 'w', encoding="utf-8") as f:
+            ch = f.write(text)
+            f.close()
+            print("File newly created")
     return(ch)
