dd94bb714abecd2e4ba2aa7685267cb5cc6f9d74
iamprosper <thinkpadprosper@gmail.com>
2022-07-06 21:01:23 +0000

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A module containing a script for adding / loading cli args to json file"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+    import sys
+    elements = []
+    filename = "add_item_.json"
+    try:
+        elements = load_from_json_file(filename)
+    except FileNotFoundError as fe:
+        save_to_json_file([], filename)
+    if (len(sys.argv) > 1):
+        for element in sys.argv[1:]:
+            elements.append(element)
+        save_to_json_file(elements, filename)
