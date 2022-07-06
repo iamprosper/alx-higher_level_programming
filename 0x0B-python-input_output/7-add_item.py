c1c903f727c7c83250de240f6c66f21c84b63208
iamprosper <thinkpadprosper@gmail.com>
2022-07-06 21:14:56 +0000

    Some tests

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,32 +1,7 @@
 #!/usr/bin/python3
 """A module containing a script for adding / loading cli args to json file"""
-# save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-# load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """ A function that save a json object to a file
-    Args:
-        -my_obj: The json object
-        -filename: The path of the file's name
-    """
-    with open(filename, 'w') as f:
-        json_file = json.dumps(my_obj)
-        f.write(json_file)
-        f.close()
-
-
-def load_from_json_file(filename):
-    """ A function that create a python object from json file
-        Args:
-            -filename: The json's file name
-    """
-    obj = None
-    with open(filename, 'r') as f:
-        obj = json.loads(f.read())
-        f.close()
-    return obj
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
