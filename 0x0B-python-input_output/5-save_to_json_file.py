2548fb74c8ccaf2f072ef8df9fd4cca502b8b0d7
iamprosper <thinkpadprosper@gmail.com>
2022-07-06 17:01:17 +0000

    Save to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A module containing a single function
+    Function name:
+        -save_to_json_file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ A function that save a json object to a file
+    Args:
+        -my_obj: The json object
+        -filename: The path of the file's name
+    """
+    json_file = json.dumps(my_obj)
+    with open(filename, 'w') as f:
+        f.write(json_file)
+        f.close()
