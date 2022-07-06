27d6d29129c7c94b424d564372c1cdfcf643d792
iamprosper <thinkpadprosper@gmail.com>
2022-07-06 13:19:53 +0000

    Task 1, write to a file, checking#3

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,7 +10,9 @@ def write_file(filename="", text=""):
             -filename: The file's name
             -text: The content to add
     """
-    with open(filename, 'w+', encoding="UTF-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.seek(0)
         ch = f.write(text)
+        f.truncate()
         f.close()
     return(ch)
