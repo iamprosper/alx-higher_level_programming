b6988783cbbacd5f2b4ea91649b16f3d74f05165
iamprosper <thinkpadprosper@gmail.com>
2022-07-08 13:43:40 +0000

    Resolving square's size validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,10 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         """The constructor"""
+        if (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size <= 0:
+            raise TypeError("size must be > 0")
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
