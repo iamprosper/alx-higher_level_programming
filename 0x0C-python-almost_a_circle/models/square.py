64abeff8ac35b96ab2e6f406cfb4df07118ed91d
iamprosper <thinkpadprosper@gmail.com>
2022-07-08 13:34:47 +0000

    Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" A module containing a single class:
+    Class:
+        -Square - Inherit from Rectangle
+"""
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ A class describin a square object.
+    It contains constructor an modifiers
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """The constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Print the content of the object"""
+        desc = "[Square] ({:d}) {:d}/{:d} - {:d}"
+        return desc.format(self.id, self.x, self.y, self.width)
