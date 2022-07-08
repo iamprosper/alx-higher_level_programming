28ed291f63a890cf02832f122960e9339d578954
iamprosper <thinkpadprosper@gmail.com>
2022-07-08 23:16:09 +0000

    Dictionary to json on base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
     Class:
         -Base
 """
+import json
 
 
 class Base:
@@ -16,3 +17,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries=[]):
+        """Json representation of list_dictionaries"""
+        if(list_dictionaries == [] or list_dictionaries is None):
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
