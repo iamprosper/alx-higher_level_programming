f39cc11776513e6465b67bdb457635232a1bf6ba
iamprosper <thinkpadprosper@gmail.com>
2022-07-12 00:51:58 +0000

    Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,9 +81,9 @@ class Rectangle(Base):
         """Display the rectangle using # character"""
         for y in range(self.y):
             print("")
-        for x in range(self.x):
-            print(' ', end="")
         for h in range(self.height):
+            for x in range(self.x):
+                print(' ', end="")
             for w in range(self.width):
                 print('#', end="")
             print("")
