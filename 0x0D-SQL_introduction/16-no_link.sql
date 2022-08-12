759942378a81f3420623df33a7dec46fc817e603
iamprosper <thinkpadprosper@gmail.com>
2022-08-12 17:08:51 +0000

    Task 16 - Reviewing#2

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- List all records of a table with restriction
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC WHERE name != NULL;
