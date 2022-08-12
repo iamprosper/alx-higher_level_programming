eabf637f100ec96eceed5c3aa78fe21e5456a9d4
iamprosper <thinkpadprosper@gmail.com>
2022-08-12 17:00:21 +0000

    Task 15 - Groups same values -reviewing#1

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- show occurences of same values
-SELECT score, count(score) as number FROM second_table GROUP BY score;
+SELECT score, count(score) as number FROM second_table GROUP BY score
+ ORDER BY score DESC;
