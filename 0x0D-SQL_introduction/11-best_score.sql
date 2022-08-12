dab6cd679aa4a9ba689b4a5c135d95729bf22014
iamprosper <thinkpadprosper@gmail.com>
2022-08-12 16:20:28 +0000

    Task 11 - conditions

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Show all the records of a table inside a database under condition
+-- This command is doing the action
+SELECT score , name FROM second_table WHERE score >= 10 ORDER BY score DESC;
