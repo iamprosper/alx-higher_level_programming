a2432fb13fec6a269febbf3459f964e1bfcda4d4
iamprosper <thinkpadprosper@gmail.com>
2022-09-06 14:41:03 +0000

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -11,9 +11,10 @@ if (typeof(process.argv[2]) == undefined){
 }
 */
 if (!parseInt(process.argv[2])) console.log('Not a number');
-else if (String(parseInt(process.argv[2])).split('').length === process.argv[2].split('').length) console.log(`My number ${process.argv[2]}`);
+else if (String(parseInt(process.argv[2])).split('').length === process.argv[2].split('').length) console.log(`My number: ${process.argv[2]}`);
+else if (String(parseFloat(process.argv[2])).split('').length === process.argv[2].split('').length) console.log(`My number: ${parseInt(process.argv[2])}`);
 else console.log('Not a number');
 /*
-if (parseInt(process.argv[2]) === NaN) console.log('Not a number');
-else console.log(`My number ${process.argv[2]}`);
+console.log(String(parseFloat(process.argv[2])).split('').length);
+console.log(process.argv[2].split('').length);
 */
