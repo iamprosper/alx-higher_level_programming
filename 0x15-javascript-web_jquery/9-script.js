e538b7e2c97d12c5d89e58ff906a81ea7a532102
iamprosper <thinkpadprosper@gmail.com>
2022-10-31 18:33:35 +0000

    Task 9 - Reviewing#1

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,5 @@
-$.getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function (data, textStatus, jqXHR) {
-  $('div#hello').text(data.hello);
+$('document').ready(function () {
+  $.getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function (data, textStatus, jqXHR) {
+    $('div#hello').text(data.hello);
+  });
 });
