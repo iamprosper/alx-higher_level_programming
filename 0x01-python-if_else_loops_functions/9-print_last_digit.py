f2a01a65433498c8dae017f01f0f90357025069a
iamprosper <thinkpadprosper@gmail.com>
2023-03-08 06:40:34 +0000

    Printing last number #4

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     str_n = str(number)[-1]
-    print(str_n, end="")
+    print("{:d}".format(str_n), end="")
     return str_n
 # print("{}".format(print_last_digit(98)))
 # print("{}".format(print_last_digit(0)))
