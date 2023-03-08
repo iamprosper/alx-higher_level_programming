7046e6149969c1e5ee80f600c1b76fb55b5b6df7
iamprosper <thinkpadprosper@gmail.com>
2023-03-08 02:40:29 +0000

    Fixing

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 for i in range(99):
-    print("{} = {}".format(i, "0x"), end="")
-    if (i < 10):
-        print("{}".format(i))
-    elif (i < 16):
-        print("{}".format(chr(i + 87)))
-    elif (i % 16 == 0):
-        print("{}0".format(i // 16))
-    else:
-        print("{:d}".format(i // 16), end="")
-        if (i % 16 >= 10):
-            print("{}".format(chr((i % 16 + 87))))
-        else:
-            print("{}".format(i % 16))
+    print("{} = {}".format(i, hex(i)))
