fe9ecd338fc3704c59902df73f3f1a1b72f270fd
iamprosper <thinkpadprosper@gmail.com>
2023-03-08 06:17:02 +0000

    Reviewing uppercase case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         uni_code = ord(str[i])
         if uni_code >= 97 and uni_code <= 122:
             uni_code = uni_code - 32
-            print("{}".format(chr(uni_code)), end='')
-            print()
+        print("{}".format(chr(uni_code)), end='')
+    print()
