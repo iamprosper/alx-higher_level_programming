5ddfdd1b72d4e9b72f66021ca44f12c5ab6d3ea9
iamprosper <thinkpadprosper@gmail.com>
2023-03-08 22:40:25 +0000

    Remove a char at index#1

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(string, n):
-    if (n >= len(string) or n < 0):
-        return
-    string = f"{string[:n]}{string[(n+1):]}"
+    if not(n >= len(string) or n < 0):
+        string = f"{string[:n]}{string[(n+1):]}"
     return string
 
 # print(remove_char_at("Best School", 3))
