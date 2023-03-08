f2402bb86fa646d2b2567597696b337bd3f71e5c
iamprosper <thinkpadprosper@gmail.com>
2023-03-08 07:01:50 +0000

    Reviewing last digit case

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    str_n = (int) str(number)[-1]
-    print("{:d}".format(str_n), end="")
+    str_n = str(number)[-1]
+    print("{}".format(str_n), end="")
     return str_n
+
 # print("{}".format(print_last_digit(98)))
-# print("{}".format(print_last_digit(0)))
+# print("{}".format(print_last_digit("Holberton")))
 # print("{}".format(print_last_digit(-1024)))
