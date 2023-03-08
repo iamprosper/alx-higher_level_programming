4348da839f619e9f7213b3cf68ea75627bde40ec
iamprosper <thinkpadprosper@gmail.com>
2023-03-08 06:29:09 +0000

    Correcting last number printing

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,3 +2,6 @@
 def print_last_digit(number):
     str_n = str(number)
     return str_n[-1]
+# print("{}".format(print_last_digit(98)))
+# print("{}".format(print_last_digit(0)))
+# print("{}".format(print_last_digit(-1024)))
