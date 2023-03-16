899c5da6decd9e4e0e240f7a0ea9325e5d5525d5
iamprosper <thinkpadprosper@gmail.com>
2023-03-16 01:54:53 +0000

    Correct output on sorting dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary)
     for item in sorted_dict:
-        print("{} : {}".format(item, a_dictionary.get(item)))
+        print("{}: {}".format(item, a_dictionary.get(item)))
