10b98ea91e6edfb5f227f49be00c9843c8f783df
iamprosper <thinkpadprosper@gmail.com>
2023-03-16 00:59:09 +0000

    Search and replace, reviewing

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
+    new_list = my_list[:]
     for i in range(len(my_list)):
         if my_list[i] == search:
-            new_list.append(replace)
-        new_list.append(my_list[i])
+            new_list[i] = replace
     return (new_list)
