fc13c4cf1b314445930c889459490eea86f8d2b9
iamprosper <thinkpadprosper@gmail.com>
2023-03-16 02:31:03 +0000

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best = ""
+    big = 0
+    if a_dictionary is not None:
+        for k, v in a_dictionary.items():
+            if v > big:
+                big = v
+                best = k
+        return (best)
+    return (None)
