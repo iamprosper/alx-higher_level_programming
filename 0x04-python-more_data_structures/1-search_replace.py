0544ed8d2ea8838f06a206855eaf27fc3bbabbc2
iamprosper <thinkpadprosper@gmail.com>
2023-03-16 00:51:44 +0000

    Reviewing task 1#1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list
+    new_list = []
     for i in range(len(my_list)):
         if my_list[i] == search:
-            new_list[i] = replace
+            new_list.append(replace)
+        new_list.append(my_list[i])
     return (new_list)
