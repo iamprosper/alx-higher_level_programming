33621ad23003c3dda53fed52b86c6feea720d782
iamprosper <thinkpadprosper@gmail.com>
2023-04-12 16:34:14 +0000

    Reviewing#2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 
 
 def add_integer(a, b=98):
+    """This function add two integers"""
     if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
     if (not isinstance(b, int) and not isinstance(b, float)):
