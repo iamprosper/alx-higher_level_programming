b3e0b0eada9cb0a80a93e999511d883715495eae
iamprosper <thinkpadprosper@gmail.com>
2023-04-21 13:43:19 +0000

    Area function not yet implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contain a single class
+   Class:
+        BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """Empty Base Geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
