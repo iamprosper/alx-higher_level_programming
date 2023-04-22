ff3142f11c09aacda188ab0a655d39019902e369
iamprosper <thinkpadprosper@gmail.com>
2023-04-22 00:33:10 +0000

    BAse square class#2

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,7 +10,7 @@ class Square(BaseGeometry):
     """Base Square class"""
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = width
+        self.__size = size
 
     def area(self):
         return (self.__size * self.__size)
