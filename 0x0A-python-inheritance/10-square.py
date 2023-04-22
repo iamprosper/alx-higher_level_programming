ce6e9ca727abc6e5d42a42cc03f391cdcfab684e
iamprosper <thinkpadprosper@gmail.com>
2023-04-22 00:46:51 +0000

    Base square class#5

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,9 +9,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Base Square class"""
     def __init__(self, size):
-        Rectangle.__init__(self, size, size)
         self.integer_validator("size", size)
         self.__size = size
+        self._Rectangle__width = size
+        self._Rectangle__height = size
 
     def area(self):
         return (self.__size * self.__size)
